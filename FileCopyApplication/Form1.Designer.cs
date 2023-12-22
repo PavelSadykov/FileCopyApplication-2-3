22c269551cc76b979167eae9eb5d18c2fe7efae5
PavelSadykov <2439747@gmail.com>
2023-12-22 15:05:33 +0500

    Добавьте файлы проекта.

---
+++ FileCopyApplication/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace FileCopyApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnBrowseSource = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnBrowseDestination = new System.Windows.Forms.Button();
+            this.txtSourcePath = new System.Windows.Forms.TextBox();
+            this.txtDestinationPath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtNumThreads = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.button = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(510, 136);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnBrowseSource
+            // 
+            this.btnBrowseSource.Location = new System.Drawing.Point(510, 52);
+            this.btnBrowseSource.Name = "btnBrowseSource";
+            this.btnBrowseSource.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseSource.TabIndex = 1;
+            this.btnBrowseSource.Text = "Source";
+            this.btnBrowseSource.UseVisualStyleBackColor = true;
+            this.btnBrowseSource.Click += new System.EventHandler(this.btnBrowseSource_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(52, 204);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(440, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // btnBrowseDestination
+            // 
+            this.btnBrowseDestination.Location = new System.Drawing.Point(510, 96);
+            this.btnBrowseDestination.Name = "btnBrowseDestination";
+            this.btnBrowseDestination.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseDestination.TabIndex = 3;
+            this.btnBrowseDestination.Text = "Destination";
+            this.btnBrowseDestination.UseVisualStyleBackColor = true;
+            this.btnBrowseDestination.Click += new System.EventHandler(this.btnBrowseDestination_Click);
+            // 
+            // txtSourcePath
+            // 
+            this.txtSourcePath.Location = new System.Drawing.Point(49, 52);
+            this.txtSourcePath.Name = "txtSourcePath";
+            this.txtSourcePath.Size = new System.Drawing.Size(443, 20);
+            this.txtSourcePath.TabIndex = 4;
+            // 
+            // txtDestinationPath
+            // 
+            this.txtDestinationPath.Location = new System.Drawing.Point(49, 96);
+            this.txtDestinationPath.Name = "txtDestinationPath";
+            this.txtDestinationPath.Size = new System.Drawing.Size(443, 20);
+            this.txtDestinationPath.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtNumThreads
+            // 
+            this.txtNumThreads.Location = new System.Drawing.Point(422, 136);
+            this.txtNumThreads.Name = "txtNumThreads";
+            this.txtNumThreads.Size = new System.Drawing.Size(70, 20);
+            this.txtNumThreads.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Введите путь к исходному файлу или нажмите кнопку Source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(327, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Введите путь для копирования или нажмите кнопку Destination";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(367, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Укажите количество потоков для копирования и нажмите кнопку Start";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(252, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Копирование";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(591, 136);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(98, 23);
+            this.buttonStop.TabIndex = 11;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(510, 166);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 12;
+            this.button.Text = "Pause";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(592, 166);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(98, 23);
+            this.btnResume.TabIndex = 13;
+            this.btnResume.Text = "Continue";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumThreads);
+            this.Controls.Add(this.txtDestinationPath);
+            this.Controls.Add(this.txtSourcePath);
+            this.Controls.Add(this.btnBrowseDestination);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnBrowseSource);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnBrowseSource;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnBrowseDestination;
+        private System.Windows.Forms.TextBox txtSourcePath;
+        private System.Windows.Forms.TextBox txtDestinationPath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtNumThreads;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button btnResume;
+    }
+}
+
---
+++ FileCopyApplication/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileCopyApplication
+{
+    public partial class Form1 : Form
+    {
+
+        private bool isPaused = false;
+        private CancellationTokenSource cancellationTokenSource;
+        private ManualResetEventSlim pauseEvent = new ManualResetEventSlim(true);
+        private TaskCompletionSource<bool> taskCompletionSource = new TaskCompletionSource<bool>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            string sourcePath = txtSourcePath.Text;
+            string destinationPath = txtDestinationPath.Text;
+
+            if (string.IsNullOrEmpty(sourcePath) || string.IsNullOrEmpty(destinationPath))
+            {
+                MessageBox.Show("Пожалуйста, выберите пути для копирования.");
+                return;
+            }
+
+            if (!File.Exists(sourcePath))
+            {
+                MessageBox.Show("Исходный файл не существует.");
+                return;
+            }
+
+            int numThreads;
+            if (!int.TryParse(txtNumThreads.Text, out numThreads) || numThreads <= 0)
+            {
+                MessageBox.Show("Введите корректное количество потоков для копирования.");
+                return;
+            }
+
+            progressBar.Minimum = 0;
+            progressBar.Value = 0;
+
+            // Получаем размер файла для вычисления прогресса
+            long fileSize = new FileInfo(sourcePath).Length;
+          
+
+            cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+
+
+            // Запускаем задачи для копирования
+            Task[] tasks = new Task[numThreads];
+            using (FileStream fsSource = new FileStream(sourcePath, FileMode.Open, FileAccess.Read))
+            {
+                for (int i = 0; i < numThreads; i++)
+                {
+                    int index = i;
+                    tasks[i] = Task.Run(() =>
+                    {
+                        long startPosition = index * (fileSize / numThreads);
+                        long endPosition = (index == numThreads - 1) ? fileSize : startPosition + (fileSize / numThreads);
+
+                        using (FileStream fsDestination = new FileStream($"{destinationPath}\\Part_{index}.dat", FileMode.Create, FileAccess.Write))
+                        {
+                            byte[] buffer = new byte[4096];
+                            int bytesRead;
+
+                            fsSource.Seek(startPosition, SeekOrigin.Begin);
+
+                            while (startPosition < endPosition && (bytesRead = fsSource.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                fsDestination.Write(buffer, 0, bytesRead);
+                                startPosition += bytesRead;
+
+                                // Обновляем прогресс в главном потоке
+                                this.Invoke((MethodInvoker)delegate
+                                {
+                                    int progress = (int)(((double)startPosition / fileSize) * 100);
+                                    progressBar.Value = progress;
+                                   
+
+                                });
+                                while (isPaused) // Приостановка копирования
+                                {
+                                    Task.Delay(100); // Ждем для обработки возможности продолжения или остановки
+                                    if (token.IsCancellationRequested)
+                                    {
+                                        return; // Остановка копирования, если запросили отмену
+                                    }
+                                }
+                            }
+                        }
+                    }, token);
+                }
+
+                    await Task.WhenAll(tasks);
+            }
+
+            if (cancellationTokenSource != null)
+            {
+                cancellationTokenSource.Dispose();
+            }
+
+            MessageBox.Show("Копирование завершено!");
+        }
+        private void btnBrowseSource_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSourcePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnBrowseDestination_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtDestinationPath.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+       //кнопка приостановить
+        private void button_Click(object sender, EventArgs e)
+        {
+            pauseEvent.Reset();
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            pauseEvent.Set();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+
+            taskCompletionSource.TrySetResult(true);
+            txtSourcePath.Clear();
+            txtDestinationPath.Clear();
+            txtNumThreads.Clear();
+            progressBar.Value = 0;
+        }
+    }
+}
